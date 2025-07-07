7d6b45fafb12ed279e50e837e60ec016db682d7d
Dobrin David <David.Dobrin@softwire.com>
2025-07-07 17:54:31 +0300

    Red-14: bishops can now take pieces, except kings and friendly ones - done

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -3,10 +3,21 @@ import Player from '../player';
 import Board from '../board';
 import Square from "../square";
 import gameSettings from "../gameSettings";
+import King from "./king";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
         super(player);
+    }
+
+    private takePieceIfPossible(board: Board, row: number, col: number, availableMoves: Square[]): Piece | undefined {
+        const pieceNearBishop: Piece | undefined = board.getPiece(new Square(row, col));
+        if (pieceNearBishop) {
+            if (pieceNearBishop.player != this.player && !(pieceNearBishop instanceof King)) {
+                availableMoves.push(new Square(row,  col));
+            }
+        }
+        return pieceNearBishop;
     }
 
     public getAvailableMoves(board: Board) {
@@ -17,8 +28,10 @@ export default class Bishop extends Piece {
         let candidatePosition = new Square(positionHelper.row, positionHelper.col);
         // forwards up diagonal
         while (candidatePosition.row < gameSettings.BOARD_SIZE - 1 && candidatePosition.col < gameSettings.BOARD_SIZE - 1) {
-            const pieceNearBishop: Piece | undefined = board.getPiece(new Square(++candidatePosition.row, ++candidatePosition.col));
-            if (pieceNearBishop) {
+            candidatePosition.row++;
+            candidatePosition.col++;
+
+            if (this.takePieceIfPossible(board, candidatePosition.row, candidatePosition.col, availableMoves)) {
                 break;
             }
             availableMoves.push(new Square(candidatePosition.row, candidatePosition.col));
@@ -26,10 +39,11 @@ export default class Bishop extends Piece {
 
         candidatePosition = new Square(positionHelper.row, positionHelper.col);
         // forwards down diagonal
-
         while (candidatePosition.row > 0 && candidatePosition.col > 0) {
-            const pieceNearBishop: Piece | undefined = board.getPiece(new Square(--candidatePosition.row, --candidatePosition.col));
-            if (pieceNearBishop) {
+            candidatePosition.row--;
+            candidatePosition.col--;
+
+            if (this.takePieceIfPossible(board, candidatePosition.row, candidatePosition.col, availableMoves)) {
                 break;
             }
             availableMoves.push(new Square(candidatePosition.row, candidatePosition.col));
@@ -38,8 +52,10 @@ export default class Bishop extends Piece {
         candidatePosition = new Square(positionHelper.row, positionHelper.col);
         // backwards up diagonal
         while (candidatePosition.row < gameSettings.BOARD_SIZE - 1 && candidatePosition.col > 0) {
-            const pieceNearBishop: Piece | undefined = board.getPiece(new Square(++candidatePosition.row, --candidatePosition.col));
-            if (pieceNearBishop) {
+            candidatePosition.row++;
+            candidatePosition.col--;
+
+            if (this.takePieceIfPossible(board, candidatePosition.row, candidatePosition.col, availableMoves)) {
                 break;
             }
             availableMoves.push(new Square(candidatePosition.row, candidatePosition.col));
@@ -48,8 +64,10 @@ export default class Bishop extends Piece {
         candidatePosition = new Square(positionHelper.row, positionHelper.col);
         // backwards down diagonal
         while (candidatePosition.row > 0 && candidatePosition.col < gameSettings.BOARD_SIZE - 1) {
-            const pieceNearBishop: Piece | undefined = board.getPiece(new Square(--candidatePosition.row, ++candidatePosition.col));
-            if (pieceNearBishop) {
+            candidatePosition.row--;
+            candidatePosition.col++;
+
+            if (this.takePieceIfPossible(board, candidatePosition.row, candidatePosition.col, availableMoves)) {
                 break;
             }
             availableMoves.push(new Square(candidatePosition.row, candidatePosition.col));
