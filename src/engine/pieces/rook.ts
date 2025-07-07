b6c1f2d5993e8335e062e07ab3ecd98d31650d52
Dobrin David <David.Dobrin@softwire.com>
2025-07-07 17:39:25 +0300

    Red-13: rooks can now take pieces, except kings and friendly ones - done

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -3,10 +3,23 @@ import Player from '../player';
 import Board from '../board';
 import Square from "../square";
 import gameSettings from "../gameSettings";
+import King from "./king";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
         super(player);
+    }
+
+    private checkMove(board: Board , row: number, col: number, availableMoves: Square[]): Piece | undefined {
+        const pieceNearRook: Piece | undefined = board.getPiece(new Square(row, col));
+        if (pieceNearRook) {
+            if (pieceNearRook.player != this.player && !(pieceNearRook instanceof King)) {
+                availableMoves.push(new Square(row,  col));
+            }
+        }
+
+        return pieceNearRook;
+
     }
 
     public getAvailableMoves(board: Board) {
@@ -15,28 +28,23 @@ export default class Rook extends Piece {
         const rookCurrentPosition: Square = board.findPiece(this);
 
         for (let i = rookCurrentPosition.row + 1; i < gameSettings.BOARD_SIZE; i++) {
-            const pieceNearRook: Piece | undefined = board.getPiece(new Square(i, rookCurrentPosition.col));
-            if (pieceNearRook) {
+            if(this.checkMove(board, i, rookCurrentPosition.col, availableMoves)) {
                 break;
             }
 
             availableMoves.push(new Square(i,  rookCurrentPosition.col));
-
         }
 
         for (let i = rookCurrentPosition.row - 1; i >= 0; i--) {
-            const pieceNearRook: Piece | undefined = board.getPiece(new Square(i, rookCurrentPosition.col));
-            if (pieceNearRook) {
+            if(this.checkMove(board, i, rookCurrentPosition.col, availableMoves)) {
                 break;
             }
 
             availableMoves.push(new Square(i,  rookCurrentPosition.col));
-
         }
 
         for (let j = rookCurrentPosition.col + 1; j < gameSettings.BOARD_SIZE; j++) {
-            const pieceNearRook: Piece | undefined = board.getPiece(new Square(rookCurrentPosition.row, j));
-            if (pieceNearRook) {
+            if(this.checkMove(board, rookCurrentPosition.row, j, availableMoves)) {
                 break;
             }
 
@@ -44,14 +52,13 @@ export default class Rook extends Piece {
         }
 
         for (let j = rookCurrentPosition.col - 1; j >= 0; j--) {
-            const pieceNearRook: Piece | undefined = board.getPiece(new Square(rookCurrentPosition.row, j));
-            if (pieceNearRook) {
+            if(this.checkMove(board, rookCurrentPosition.row, j, availableMoves)) {
                 break;
             }
-
             availableMoves.push(new Square(rookCurrentPosition.row,  j));
         }
 
         return availableMoves;
     }
+
 }
