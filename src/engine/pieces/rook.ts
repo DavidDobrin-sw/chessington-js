776c3b944aea5e1cfc5c8ee183df62249920567e
Dobrin David <David.Dobrin@softwire.com>
2025-07-07 14:56:33 +0300

    Red-3 done

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import gameSettings from "../gameSettings";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,21 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+
+        let availableMoves: Square[] = [];
+        const rookCurrentPosition: Square = board.findPiece(this);
+        for (let i = 0; i < gameSettings.BOARD_SIZE; i++) {
+            if(i != rookCurrentPosition.row) {
+                availableMoves.push(new Square(i,  rookCurrentPosition.col));
+            }
+        }
+
+        for (let j = 0; j < gameSettings.BOARD_SIZE; j++) {
+            if( j != rookCurrentPosition.col) {
+                availableMoves.push(new Square(rookCurrentPosition.row,  j));
+            }
+        }
+
+        return availableMoves;
     }
 }
