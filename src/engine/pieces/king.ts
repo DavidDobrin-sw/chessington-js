3003daea31ed7d34c23390d5b5af66a8866a326f
Dobrin David <David.Dobrin@softwire.com>
2025-07-07 15:59:35 +0300

    Red-7 done

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,15 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableMoves: Square[] = [];
+        const kingInitialPosition: Square = board.findPiece(this);
+
+        const kingMoves: number[][] = [[0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1], [1, 0], [1, 1]];
+
+        for (let i = 0; i < kingMoves.length; i++) {
+            availableMoves.push(new Square(kingInitialPosition.row + kingMoves[i][0], kingInitialPosition.col + kingMoves[i][1]));
+        }
+
+        return availableMoves;
     }
 }
